36e6c90993e5b944a17e606f8b17d6779ce21fe3
w.souza <w.souza@fliegersoftware.de>
2016-05-24 16:52:07 -0300

    add compass config

---
+++ config.rb
@@ -0,0 +1,11 @@
+require 'compass/import-once/activate'
+
+http_path = "/"
+sass_dir = "src"
+css_dir = "assets/css"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
+
+output_style = :expanded
+relative_assets = true
+line_comments = false
